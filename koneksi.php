706f47b4a17fbe0e94567970bd0e6d278a4e8589
userKean <145644407+userKean@users.noreply.github.com>
2023-09-22 17:05:07 +0700

    a

--- koneksi.php
+++ koneksi.php
@@ -3,7 +3,7 @@
 $server = "hosting"; // Lokasi server database (biasanya localhost)
 $username = "root"; // Username MySQL (biasanya root)
 $password = ""; // Password MySQL (biarkan kosong jika tidak ada)
-$database = "test1"; // Nama database yang ingin Anda hubungkan
+$database = "data1"; // Nama database yang ingin Anda hubungkan
 
 // Membuat koneksi ke database
 $conn = new mysqli($server, $username, $password, $database);
@@ -12,3 +12,4 @@ $conn = new mysqli($server, $username, $password, $database);
 if ($conn->connect_error) {
     die("Koneksi ke database gagal: " . $conn->connect_error);
 }
+?>
