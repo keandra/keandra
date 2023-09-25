03896ba76956aae0a40bf6090d54fac21e26b2c9
userKean <145644407+userKean@users.noreply.github.com>
2023-09-25 16:58:31 +0700

    abc

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,6 @@
 <?php
 // Konfigurasi database
-$server = "userKean"; // Lokasi server database (biasanya localhost)
+$server = "localhost"; // Lokasi server database (biasanya localhost)
 $username = "root"; // Username MySQL (biasanya root)
 $password = ""; // Password MySQL (biarkan kosong jika tidak ada)
 $database = "data1"; // Nama database yang ingin Anda hubungkan
@@ -12,4 +12,16 @@ $conn = new mysqli($server, $username, $password, $database);
 if ($conn->connect_error) {
     die("Koneksi ke database gagal: " . $conn->connect_error);
 }
+
+$nama = $_REQUEST['food'];
+
+$sql = "INSERT INTO test1 (id, makanan) VALUES (' ', '$nama')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Data berhasil dimasukkan ke dalam database.";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
 ?>
